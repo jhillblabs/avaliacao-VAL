98f71f1193e83803c2e79c96b2bb3e129a79673a
jhillblabs <jhilzinhavendito@gamil.com>
2024-09-17 20:37:04 -0300

    conseguiu metade do objetivo, mas falta botão de clicar e ver o resultado

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ListaUsuarioScreen from './components/ListaUsuarioScreen';
